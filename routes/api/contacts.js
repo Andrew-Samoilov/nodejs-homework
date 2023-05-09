06235105d4e118c4b9e74ae2150177aa9a82bc2f
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2023-05-09 14:25:14 +0300

    GET works

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,25 @@
-const express = require('express')
+const express = require('express');
+
+const contacts = require("../../models/contacts");
 
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get("/", async (_, res) => {
+  try {
+    const allContacts = await contacts.listContacts();
+    res.status = "success";
+    res.code = 200;
+    res.json(allContacts);
+  }
+  catch (error) {
+    res.status(500).json({
+      message: "Server error"
+    })
+  }
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'contactId template message' })
 })
 
 router.post('/', async (req, res, next) => {
